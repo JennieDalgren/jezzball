1c15920462c128209c26bb62bfd844884979e96f
Jennie Dalgren <jennie.dalgren@gmail.com>
2017-08-29 16:02:47 +0200

    version 3 - constructors

--- game2.js
+++ game2.js
@@ -1,4 +1,8 @@
 
+
+
+
+
 var canvas = document.getElementById("myCanvas");
 var c = canvas.getContext("2d");
 
--- game3.js
+++ game3.js
@@ -1,2 +1,49 @@
-var canvas = document.getElementById("myCanvas");
-var c = canvas.getContext("2d");
+
+
+// CANVAS CONSTRUCTOR //
+// ================== //
+
+function Canvas () {
+  this.level = 1;
+  this.w = 600;
+  this.h = 400;
+  this.balls = [];
+
+  this.generateBalls();
+}
+
+Canvas.prototype.generateBalls = function () {
+  for (var i = 0; i < this.level; i++){
+    this.balls.push(new Ball());
+  }
+};
+
+
+// BALL  CONSTRUCTOR //
+// ================== //
+
+function Ball () {
+  this.position = {
+    x: 0,
+    y: 0
+  };
+  this.radius = 10;
+  this.speed = {
+    x: 3,
+    y: 3
+  };
+
+}
+
+
+
+
+
+// INITIALIZE JQUERY  //
+// ================== //
+var canvas;
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    canvas = new Canvas();
+});
